8d74bc7b50b5e0f999aa76620fe955f71619923a
Shreyas Jain <shreyas18jan@gmail.com>
2021-03-03 18:12:11 +0530

    Initial changes to the Cucumber framework example

---
+++ src/main/java/Runner/RunTest.java
@@ -0,0 +1,15 @@
+package Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/main/java/Features",
+        glue={"StepDefinition"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"})
+public class RunTest
+{
+
+}
